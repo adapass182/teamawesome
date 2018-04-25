e50cac7b65c4de26fde337c3ef68497ec9e347b5
Zsuzsi Hudacsko <zsuzsi.hudacsko@gmail.com>
2018-04-25 13:05:04 +0200

    more html and js

---
+++ main.js
@@ -0,0 +1,7 @@
+var today = new Date(2018, 04, 25, 12, 49);
+var hourNow = today.getHours();
+var dateToday = today.getDate();
+var time;
+
+document.write('<h4>' + time + '</h4>')
+//document.write('<div class="currentdate">')
